9ecbb88a2bfd5263ebc90ab0958c66f02aa93912
Olb2 <olgaborysovska@gmail.com>
2020-07-30 12:31:07 -0700

    setting up the express

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 //this is a BIGG test!
 //please ignore if I do something fully incorrect thank you :)
+const express = require('express');
+
+const app = express();
+
+//routes 
+app.get('/home', (req, res)=> {
+    res.render('home.ejs')
+})
+
+//listen 
+const PORT = 3000
+app.listen(PORT, ()=>{
+	console.log(`'listening.... ${PORT}'`);
+})
